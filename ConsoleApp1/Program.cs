6165b9e1646ab3f48b053c052bbca2d9e6eb5269
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-09-13 15:15:10 +0200

    Dodano test dla klasy inicjalizującej

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace SalesTaxesUSTDD
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
--- TestyHednostkowe/ArrayInitializerTest.cs
+++ TestyHednostkowe/ArrayInitializerTest.cs
@@ -6,9 +6,23 @@ namespace TestyHednostkowe
     [TestClass]
     public class ArrayInitializerTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static Object[] arrayToInitialize;
+
+        [TestInitialize]
+        public void setUpProducts()
         {
+            arrayToInitialize = Initializer.InitializeArray<Object>(10);
+        }
+
+        [TestMethod]
+        public void inititializedArrayShouldNotContainAnyNull()
+        {
+            Object temp = null;
+            for (int i = 0; i < arrayToInitialize.GetLength(0); i++)
+            {
+                temp = arrayToInitialize[i];
+                Assert.IsNotNull(temp);
+            }
         }
     }
 }
