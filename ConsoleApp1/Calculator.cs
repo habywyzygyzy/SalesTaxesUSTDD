871928fba40c5101b17ddef0936607b42ebed9ad
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-09-13 16:06:03 +0200

    Dodano klasę Calculator

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTaxesUSTDD
+{
+    public static class Calculator
+    {
+        public const float BASIC_MARGIN = 0.1F;
+        public const float PERCENT = 0.01F;
+
+        public static float calculateTax(float price, float tax)
+        {
+            float calculatedTax = price * tax * PERCENT;
+            calculatedTax = roundDown(calculatedTax);
+            return calculatedTax;
+        }
+        public static float calculateBasicMargin(float price)
+        {
+            float priceWithMargin = price * BASIC_MARGIN;
+            priceWithMargin = roundDown(priceWithMargin);
+            return priceWithMargin;
+        }
+        public static float roundDown(float valueToRound)
+        {
+            valueToRound = (float)Math.Floor(valueToRound * 100) / 100;
+            return valueToRound;
+        }
+    }
+}
